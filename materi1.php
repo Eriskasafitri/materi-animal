a274cda5cfa89710ec27171f20ec4dfae3fed262
Eriskasafitri <71135116+Eriskasafitri@users.noreply.github.com>
2020-09-11 20:58:56 +0700

    Add files via upload

---
+++ materi1.php
@@ -0,0 +1,103 @@
+<?php 
+
+class hewan {
+	public $judul;
+}
+	$judul = new hewan;
+	$judul->awal = "Animal";
+
+	echo "$judul->awal";
+	echo "<hr>";
+
+
+	class Kucing {
+		public $nama, $jumlah_kaki, $bisa, $suara;
+	}
+
+	$Kucing = new Kucing;
+	$Kucing->nama = "Kucing";
+	$Kucing->jumlah_kaki = "4";
+	$Kucing->bisa = "tidak bisa terbang";
+	$Kucing->suara = "Meooong";
+
+	echo "zelo adalah $Kucing->nama <br>";
+	echo "punya kaki sebanyak : $Kucing->jumlah_kaki <br>";
+	echo "$Kucing->bisa <br>";
+	echo "bersuara : $Kucing->suara";
+	echo "<hr>";
+
+	
+
+	class Anjing {
+		public $nama, $jumlah_kaki, $bisa, $suara;
+	}
+
+	$Anjing = new Anjing;
+	$Anjing->nama = "Anjing";
+	$Anjing->jumlah_kaki = "4";
+	$Anjing->bisa = "tidak bisa terbang";
+	$Anjing->suara = "Guk Guk";
+
+	echo "zela adalah $Anjing->nama <br>";
+	echo "punya kaki sebanyak : $Anjing->jumlah_kaki <br>";
+	echo "$Anjing->bisa <br>";
+	echo "bersuara : $Anjing->suara";
+	echo "<hr>";
+
+
+
+	class Sapi {
+		public $nama, $jumlah_kaki, $bisa, $suara;
+	}
+
+	$Sapi = new Sapi;
+	$Sapi->nama = "Sapi";
+	$Sapi->jumlah_kaki = "4";
+	$Sapi->bisa = "tidak bisa terbang";
+	$Sapi->suara = "Moooooo";
+
+	echo "zeli adalah $Sapi->nama <br>";
+	echo "punya kaki sebanyak : $Sapi->jumlah_kaki <br>";
+	echo "$Sapi->bisa <br>";
+	echo "bersuara : $Sapi->suara";
+	echo "<hr>";
+
+
+
+
+	class Angsa {
+		public $nama, $jumlah_kaki, $bisa, $suara;
+	}
+
+	$Angsa = new Angsa;
+	$Angsa->nama = "Angsa";
+	$Angsa->jumlah_kaki = "2";
+	$Angsa->bisa = "bisa terbang";
+	$Angsa->suara = "Kwaaak";
+
+	echo "zero adalah $Angsa->nama <br>";
+	echo "punya kaki sebanyak : $Angsa->jumlah_kaki <br>";
+	echo "$Angsa->bisa <br>";
+	echo "bersuara : $Angsa->suara";
+	echo "<hr>";
+
+
+
+
+	class Kambing {
+		public $nama, $jumlah_kaki, $bisa, $suara;
+	}
+
+	$Kambing = new Kambing;
+	$Kambing->nama = "Kambing";
+	$Kambing->jumlah_kaki = "4";
+	$Kambing->bisa = "tidak bisa terbang";
+	$Kambing->suara = "Mbeeeeekk";
+
+	echo "zili adalah $Kambing->nama <br>";
+	echo "punya kaki sebanyak : $Kambing->jumlah_kaki <br>";
+	echo "$Kambing->bisa <br>";
+	echo "bersuara : $Kambing->suara";
+	echo "<hr>";
+
+ ?>
